79c08bbf7095646f163eb4fae5666ed5c277f85a
ssoima <sergiu.soima@gmail.com>
2014-05-17 20:23:12 +0200

    parse angefangen

--- src/com/ccuhack24/angrycars/testing/Events.java
+++ src/com/ccuhack24/angrycars/testing/Events.java
@@ -1,5 +1,6 @@
 package com.ccuhack24.angrycars.testing;
 
+import java.util.Date;
 import java.util.Set;
 
 public class Events {
@@ -24,6 +25,23 @@ public class Events {
 		allplayer.remove(curentplayer);
 		allplayer.add(curentplayer);
 		return allplayer;	
+	}
+	/**
+	 * returns double[3]: 0-year
+	 * 					  1-month
+	 * 					  2-day
+	 * 
+	 */
+	private double[3] parseTimeStamp(String s)
+	{	
+		int[] parseDate= new int[6];
+		int i; 
+		for(in i )
+		parseDate[0]=s.substring(s.indexOf("123456789"),s.indexOf+3 );
+		Date date = new Date(year, month, day, hour, minute, second)
+		double[] date = new double[3];
+		return null;
+				
 	}
 	
 }
